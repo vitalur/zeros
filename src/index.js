a5ba01b63e78ddb4f711886865da01449bb7cfe6
vitalur <39513998+vitalur@users.noreply.github.com>
2018-09-24 20:09:37 +0300

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -1,14 +1,9 @@
 module.exports = function getZerosCount(number) {
-  var first = Math.floor(number / 5);
-  var second = Math.floor(first / 5);
-  var third = Math.floor(second / 5);
-  var four = Math.floor(third / 5);
-  var five = Math.floor(four / 5);
-  var six = Math.floor(five / 5);
-  var seven = Math.floor(six / 5);
-  var eight = Math.floor(seven / 5);
-  var nine = Math.floor(eight / 5);
-  var ten = Math.floor(nine / 5);
-  var eleven = Math.floor(ten / 5);
-  return first + second + third + four + five + six + seven + eight + nine + ten + eleven;
+  	let first = Math.floor(number / 5);
+  	let count = first;
+	for (let i = 0; i < 11; i++) {
+		first = Math.floor(first / 5);
+		count += first;
+	}
+	return count;
 }
